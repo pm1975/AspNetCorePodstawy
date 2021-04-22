d2c0051807b9bc01d229dbe074c6a1f246f35766
PM <pm1975@windowslive.com>
2021-04-22 12:30:24 +0200

    First commit

---
+++ ProjectAngular/ProjectAngular/Controllers/KursController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAngular.Controllers
+{
+    [ApiController]
+    [Route("kurs")]
+    public class KursController : ControllerBase
+    {
+        public KursController()
+        {
+        }
+
+    }
+}
